941a43fc1bad29fa5109cc879b621aa464d98bde
あんでぃー <bboy.answer.d@gmail.com>
2019-09-25 23:31:03 +0900

    moduleの作成テスト

---
+++ library/my_test_module.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: my_test
+
+short_description: This is my test module
+
+version_added: "2.8"
+
+description:
+    - "This is my longer description explaining my test module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the test module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - Your Name (@yourhandle)
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  my_test:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_test:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_test:
+    name: fail me
+'''
+
+RETURN = '''
+original_message:
+    description: The original name param that was passed in
+    type: str
+    returned: always
+message:
+    description: The output message that the test module generates
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    module_args = dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False),
+    )
+
+    result = dict(
+        changed=False,
+        original_message='',
+        message='',
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+    )
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    result['original_message'] = module.params['name']
+    result['message'] = 'unko!!'
+
+    if module.params['new']:
+        result['changed'] = True
+
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    module.exit_json(**result)
+
+
+def main():
+    print("hello ansible module!")
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
